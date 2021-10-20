96140220dda7ab0ad9b7d33e55c944fc44ec63cf
ShirkerZ <zubenkodevelop@gmail.com>
2021-10-20 10:20:20 +0200

    added DatePicker component

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,41 +1,47 @@
 <template>
-  <div class="select-date-container">
+  <div class="date-picker-container">
     <f7-list>
       <f7-list-item
-        :title="selectedDate.label"
+        :title="selectedDate"
         smart-select
-        :smart-select-params="{ openIn: 'sheet' }"
+        :smart-select-params="{
+          openIn: 'sheet',
+          sheetSwipeToClose: true,
+          sheetCloseLinkText: 'Chiudi',
+          closeOnSelect: true,
+        }"
+        smart-select-init
       >
-        <select name="datePicker" v-model="selectedDate">
+        <select name="datePicker" v-model="selectedDate" @change="dateSelected">
           <option
-            :value="day"
+            :value="day.date"
             v-for="(day, index) of week"
             :key="index"
-            :selected="index == 0"
+            :selected="index === 3"
           >
             {{ day.label }}
           </option>
         </select>
       </f7-list-item>
     </f7-list>
-
-    {{ selectedDate }}
   </div>
 </template>
 
 <script>
+import store from "../js/store.js";
+
 export default {
   data() {
     return {
       selectedDate: [],
       days: [
+        "Domenica",
         "Lunedì",
         "Martedì",
         "Mercoledì",
         "Giovedì",
         "Venerdì",
         "Sabato",
-        "Domenica",
       ],
       months: [
         "Gennaio",
@@ -54,6 +60,12 @@ export default {
     };
   },
 
+  methods: {
+    dateSelected() {
+      store.dispatch("fetchSchedule", this.selectedDate);
+    },
+  },
+
   computed: {
     week() {
       let week = [];
@@ -64,7 +76,7 @@ export default {
         let day = current.getDate();
         let year = current.getFullYear();
         let date = [year, month, day].join("-");
-        let label = `${this.days[current.getDay() - 1]} ${day} ${
+        let label = `${this.days[current.getDay()]} ${day} ${
           this.months[month - 1]
         }`;
 
@@ -74,8 +86,17 @@ export default {
         });
         current.setDate(current.getDate() + 1);
       }
+      this.selectedDate = week[0].date;
       return week;
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.date-picker-container {
+  .list {
+    margin-bottom: 0;
+  }
+}
+</style>
